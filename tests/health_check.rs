9b46dab0d9ed73ceed06f8d84d1afd02d6711812
jdtaylor311 <jdtaylor311@outlook.com>
2024-04-15 08:19:58 -0400

    Added sink to tracing for tests and main

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,8 +16,19 @@ use zero2prod::telemetry::*;
 
 //Ensure that the `tracing` stack is only initialised once using `once_cell`
 static TRACING: Lazy<()> = Lazy::new(|| {
-    let subscriber = get_subscriber("test".into(), "debug".into());
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    // We cannot assign the output of `get_subscriber` to a variable based on the
+    // value of TEST_LOG` becauset the sink is part of the type returned by
+    // `get_subscriber`, therefore ther are not the same type. We could work around it,
+    // but this is the most straight-forward way of moving forward.
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber)
+    } else {
+        let subscriber = get_subscriber("test".into(), "debug".into(), std::io::sink);
+        init_subscriber(subscriber);
+    }
 });
 
 pub struct TestApp {
