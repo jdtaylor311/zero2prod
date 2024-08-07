8b12248bb6307db190dc59be8c311eac70f58136
jdtaylor311 <jdtaylor311@outlook.com>
2024-08-07 15:50:59 -0400

    correct script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,17 +1,17 @@
-#!/opt/homebrew/bin/bash
+#!/bin/bash
 
 set -x
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
-	echo >&2 "Error: psql is not installed."
-	exit 1
+  echo >&2 "Error: psql is not installed."
+  exit 1
 fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
-	echo >&2 "Error: sqlx is not installed."
-	echo >&2 "Use cargo install --version='~0.7' sqlx-cli --no-default-features --features rustlt,postgres"
-	exit 1
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use cargo install --vension='~0.7' sqlx-cli --no-default-features --features rustls,postgres"
+  exit 1
 fi
 
 # Check if a custom user has been set, otherwile default to 'postgres'
@@ -28,24 +28,24 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 # Allow to skip Docker if a dockerized Postgres database is already running
 
 if [[ -z "$SKIP_DOCKER" ]]; then
-	# Launch postgres using Docker
-	docker run \
-		-e POSTGRES_USER="$DB_USER" \
-		-e POSTGRES_PASSWORD="$DB_PASSWORD" \
-		-e POSTGRES_DB="$DB_NAME" \
-		-p "$DB_PORT":5432 \
-		-d postgres \
-		postgres -N 1000
+  # Launch postgres using Docker
+  docker run \
+    -e POSTGRES_USER="$DB_USER" \
+    -e POSTGRES_PASSWORD="$DB_PASSWORD" \
+    -e POSTGRES_DB="$DB_NAME" \
+    -p "$DB_PORT":5432 \
+    -d postgres \
+    postgres -N 1000
 #   ^ Increased max number of connections for testing purposes
 fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="$DB_PASSWORD"
 until
-	psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "postgres" -c '\q'
+  psql -h "$DB_HOST" -U "$DB_USER" -p "$DB_PORT" -d "postgres" -c '\q'
 do
-	>&2 echo "Postgres is still unavailable - sleeping"
-	sleep 1
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
 done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
